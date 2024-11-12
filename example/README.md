f4e2982a824aada0d2cd54d05d1213997d7e1e18
Duy Le Ngoc <askbills@gmail.com>
2024-11-12 14:54:01 +0700

    perf: make script more automatically

--- example/README.md
+++ example/README.md
@@ -4,12 +4,23 @@ As a best-practice, we always verify our package before publishing your package 
 
 ## How to verify your customized package on your local machine
 
+### Setup credentials
+
+Create the `.env` file from `.env.example` if not present and configure necessary credentials
+
+```bash
+cp -R ./.env.example ./.env
+```
+
 ### Reuse the `example` in our case
 
 Trigger start to install the local package and run your logic inside `index.js`
 
 ```bash
+# Simulate Myria generate whitelist and approve whitelist on-chain by developer
 npm run start
+# Simulate End-User interact with Myria ecosystem to claim on-chain
+npm run claim:reward
 ```
 
 ### To verify a new testing_project
--- example/src/claim-reward.js
+++ example/src/claim-reward.js
@@ -1,4 +1,4 @@
-import { Client, Transaction, Wallet } from '@myria/airdrop-js';
+import { Client, Transaction, Type, Wallet } from '@myria/airdrop-js';
 import { API } from '@myria/api-request';
 import { Signature } from '@myria/crypto-js';
 import dotenv from 'dotenv';
@@ -16,107 +16,127 @@ dotenv.config();
     );
 
     // End user developer
-    const userWalletId = process.env.USER_WALLET_ID; // User role Wallet
-    const xApiKey = process.env.X_API_KEY;
-
+    const userClaimingWalletAddress = process.env.USER_WALLET_ID; // User role Wallet
     const ethPrivateKey = process.env.USER_ETH_PRIVATE_KEY; // User role
+
     // Thirdweb
+    const claimTokenAddress = process.env.MYRIA_CLAIM_TOKEN_ADDRESS;
     const airDropTokenAddress = process.env.AIR_DROP_TOKEN_ADDRESS;
-    const apiSecretKey = process.env.THIRD_WEB_SECRET_KEY;
+    const apiThirdwebSecretKey = process.env.THIRD_WEB_SECRET_KEY;
+    const selectedChain = Type.SupportingChain.SEPOLIA;
 
     // Myria
+    const claimingLeaderboardId = Number(process.env.LEADERBOARD_ID);
+    const myriaClientApiKey = process.env.X_API_KEY;
+    const myriaPrivateStarkKey = process.env.MYRIA_PRIVATE_STARK_KEY;
+    const myriaPublicStarkKey = process.env.MYRIA_PUBLIC_STARK_KEY;
 
-    const privateStarkKey = process.env.MYRIA_PRIVATE_STARK_KEY;
-    const publicStarkKey = process.env.MYRIA_PUBLIC_STARK_KEY;
-    const myriaClaimTokenAddress = process.env.MYRIA_CLAIM_TOKEN_ADDRESS;
-
+    // 1. Fetching Myria's Available rewards
     const res = await resourceApiService.get(
-        `/v1/rewards/by-user/${userWalletId}`,
-        { leaderboardId: 189 },
+        `/v1/rewards/by-user/${userClaimingWalletAddress}`,
+        { leaderboardId: claimingLeaderboardId },
         {
-            'x-api-key': xApiKey,
+            'x-api-key': myriaClientApiKey,
         },
     );
     const { items } = res.data.data;
     const rewardIds = items.map((i) => i.reward.id);
     console.log('---->rewardIds', rewardIds);
+
+    // 2. Fetching claim info by invoking Myria's Pre-claim
     // Build signature
     let timestamp = new Date().getTime();
     // User role (End user) info
     let signatureResult = Signature.generateHeaderSignatureFromTimestamp(
-        privateStarkKey,
-        publicStarkKey,
+        myriaPrivateStarkKey,
+        myriaPublicStarkKey,
         timestamp,
-        `/v1/rewards/pre-claim/by-user/${userWalletId}`,
+        `/v1/rewards/pre-claim/by-user/${userClaimingWalletAddress}`,
     );
-    // Pre claim
     const resPreClaim = await resourceApiService.patch(
-        `/v1/rewards/pre-claim/by-user/${userWalletId}`,
+        `/v1/rewards/pre-claim/by-user/${userClaimingWalletAddress}`,
         {
-            leaderboardId: 189,
+            leaderboardId: claimingLeaderboardId,
             rewardIds,
         },
         {
-            'x-api-key': xApiKey,
+            'x-api-key': myriaClientApiKey,
             'x-signature': signatureResult['x-signature'],
             'x-timestamp': signatureResult['x-timestamp'].toString(),
             'Stark-Key': signatureResult['stark-key'],
         },
     );
 
+    // 3. Invoke on-chain claim with Ethereum account
     const { getThirdwebContract } = Transaction;
     const { createThirdwebClientWithSecretKey } = Client;
 
-    console.log('[getThirdwebContract]: apiSecretKey = ' + apiSecretKey);
-    const client = createThirdwebClientWithSecretKey(apiSecretKey);
+    console.log(
+        '[getThirdwebContract]: apiSecretKey = ' + apiThirdwebSecretKey,
+    );
+    const client = createThirdwebClientWithSecretKey(apiThirdwebSecretKey);
 
     console.log('---> client', JSON.stringify(client));
 
     const { privateKeyToAccount } = Wallet;
-    const ownerContractAccount = privateKeyToAccount({
+    const endUserAccount = privateKeyToAccount({
         client,
         privateKey: ethPrivateKey,
     });
-    console.log('ownerContractAccount', JSON.stringify(ownerContractAccount));
-    const { address: addressUserClaim } = ownerContractAccount;
+    console.log('endUserAccount', JSON.stringify(endUserAccount));
+    const { address: addressUserClaim } = endUserAccount;
     // Claim Air
     // 2. Get airdrop contract
     const airdropContract = getThirdwebContract(
         airDropTokenAddress,
         client,
-        'sepolia',
+        selectedChain,
     );
     console.log(
         '[getThirdwebContract]: airdropContract = ' +
             JSON.stringify(airdropContract),
     );
+    let transactionReceipt;
+    let errorMessage;
     try {
-        const transactionReceipt = await Transaction.claimAirdropToken(
-            myriaClaimTokenAddress, // MyriaTestToken
-            addressUserClaim.toLocaleLowerCase(), //
+        transactionReceipt = await Transaction.claimAirdropToken(
+            claimTokenAddress, // MyriaTestToken
+            endUserAccount, //
             airdropContract,
         );
         console.log(JSON.stringify(transactionReceipt));
     } catch (error) {
+        errorMessage = error.message;
         console.log('--->>>>>', JSON.stringify(error));
     }
 
-    signatureResultPostClaim = Signature.generateHeaderSignatureFromTimestamp(
-        VALID_PRIVATE_STARK_KEY,
-        VALID_STARK_KEY,
-        timestamp,
-        `/v1/rewards/post-claim/by-user/${userWalletId}`,
-    );
+    // 4. Post-claim: Sync on-chain with Myria's service
+    const signatureResultPostClaim =
+        Signature.generateHeaderSignatureFromTimestamp(
+            myriaPrivateStarkKey,
+            myriaPublicStarkKey,
+            timestamp,
+            `/v1/rewards/post-claim/by-user/${userClaimingWalletAddress}`,
+        );
+    const transactionHash = transactionReceipt
+        ? transactionReceipt.transactionHash
+        : undefined;
+    const payloadPostClaim = {
+        rewardIds,
+    };
+    if (transactionHash) {
+        // submit the transactionHash to map as CLAIMED
+        payloadPostClaim['transactionHash'] = transactionHash;
+    } else {
+        // submit with error; to revert status to AVAILABLE
+        payloadPostClaim['errorMessage'] = errorMessage || 'Unknown';
+    }
     // Pre claim
-    const resPostClaim = await resourceApiService.post(
-        `/v1/rewards/post-claim/by-user/${userWalletId}`,
+    const resPostClaim = await resourceApiService.patch(
+        `/v1/rewards/post-claim/by-user/${userClaimingWalletAddress}`,
+        payloadPostClaim,
         {
-            rewardIds: ['string'],
-            transactionHash: 'string',
-            errorMessage: 'string',
-        },
-        {
-            'x-api-key': xApiKey,
+            'x-api-key': myriaClientApiKey,
             'x-signature': signatureResultPostClaim['x-signature'],
             'x-timestamp': signatureResultPostClaim['x-timestamp'].toString(),
             'Stark-Key': signatureResultPostClaim['stark-key'],
